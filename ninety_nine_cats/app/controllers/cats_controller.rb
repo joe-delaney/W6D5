ad05a4fec83446bbaf5bb8354b039fc48329089c
App Academy Student <email@email.com>
2022-03-11 14:55:25 -0500

    finished new

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -12,4 +12,24 @@ class CatsController < ApplicationController
     render :show
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render json: @cat.errors.full_messages
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+  end
+
 end
