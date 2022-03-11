c98a191693e310c26c844f8e94c15a4b61e362e2
App Academy Student <email@email.com>
2022-03-11 15:35:51 -0500

    finished partial for new/edit

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -13,6 +13,8 @@ class CatsController < ApplicationController
   end
 
   def new
+    @cat = Cat.new
+    
     render :new
   end
 
@@ -20,10 +22,27 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
+      redirect_to cats_url
+    else
+      render json: @cat.errors.full_messages
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+
+    render :edit
+  end
+
+  def update 
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
       redirect_to cat_url(@cat)
     else
       render json: @cat.errors.full_messages
     end
+
   end
 
   private
