a6ce0734235c5885aa02a77986257c4938c3dee4
App Academy Student <email@email.com>
2022-03-11 14:22:19 -0500

    finished index/show pages

--- ninety_nine_cats/config/routes.rb
+++ ninety_nine_cats/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cats, only: [:index, :show, :new, :create, :edit, :update]
 end
--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+
+c1 = Cat.create!(name: 'jerry', color: 'grey', sex: 'M', birth_date: '2000/08/27', description: 'I am a mouse')
+c2 = Cat.create!(name: 'gargfield', color: 'orange', sex: 'M', birth_date: '2009/08/27', description: 'Eating lasagna')
+c3 = Cat.create!(name: 'bro', color: 'black', sex: 'F', birth_date: '1999/08/27', description: 'I am a bro')
+c4 = Cat.create!(name: 'mom', color: 'grey', sex: 'F', birth_date: '1900/06/27', description: 'I am a mom')
